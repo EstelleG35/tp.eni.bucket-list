70ab038728c7a8d5fd17c4826850d105c245f7b5
estelle.g <estelleguillet35@gmail.com>
2022-04-19 11:09:34 +0200

    list and detail idea 11h08

---
+++ src/Controller/IdeaController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Idea;
+use App\Repository\IdeaRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IdeaController extends AbstractController
+{
+    /**
+     * @Route("/idea/list", name="app_list")
+     */
+    public function list(EntityManagerInterface $emi): Response
+    {
+        // récupérer la liste en BDD
+        $ideaRepo = $emi->getRepository(Idea::class);
+
+        //on trie les différents titres d'idées selon leur dates de publication, du plus récent au plus ancien.
+        $ideas = $ideaRepo->findBy([],['dateCreated' => "DESC"]);
+
+        // on envoie les données à twig
+        return $this->render('idea/list.html.twig', [
+            'ideas' => $ideas
+        ]);
+    }
+
+    /**
+     * @Route("/idea/detail/{id}", name="app_detail")
+     */
+    public function detail(Idea $idea): Response
+    {
+        //l'id sera filtrer grâce à la route
+        //on envoie les données à twig
+        return $this->render('idea/detail.html.twig', [
+            'idea' => $idea
+        ]);
+    }
+}
---
+++ src/Controller/MainController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function home(): Response
+    {
+        return $this->render("main/home.html.twig");
+    }
+
+    /**
+     * @Route("/about-us", name="app_about_us")
+     */
+    public function aboutUs(): Response
+    {
+        return $this->render("main/about-us.html.twig");
+    }
+
+}
